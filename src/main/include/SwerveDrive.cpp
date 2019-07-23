7b5e5ec36fd7b855b9110eb4eed04d9595765627
5GamAzing <ajdailey5@gmail.com>
2019-07-23 12:59:11 -0400

    Account for Hall Effect sensors going negative

--- src/main/include/SwerveDrive.cpp
+++ src/main/include/SwerveDrive.cpp
@@ -4,8 +4,8 @@
 #include "Robot.h"
 #include "JoystickControls.cpp"
 
-#define speed 0.5
-#define encoders 48
+static const float SPEED = 0.5;
+static const int ENCODERS = 48;
 
 
 /*
@@ -17,6 +17,16 @@ Modes:
 4 = 1+3
 */
 void Robot::swerveDrive(int mode) {
+    //Keep hall values between -ENCODERS and ENCODERS
+    if ((*flHall).Get() <= -ENCODERS || (*flHall).Get() >= ENCODERS)
+        (*flHall).Reset();
+    if ((*frHall).Get() <= -ENCODERS || (*frHall).Get() >= ENCODERS)
+        (*frHall).Reset();
+    if ((*blHall).Get() <= -ENCODERS || (*blHall).Get() >= ENCODERS)
+        (*blHall).Reset();
+    if ((*brHall).Get() <= -ENCODERS || (*brHall).Get() >= ENCODERS)
+        (*brHall).Reset();
+    
     //Calculate stuff
     float lAngle = angleCalc((*control1).GetX(LHand), 0-(*control1).GetY(LHand));
     float rAngle = angleCalc((*control1).GetX(RHand), 0-(*control1).GetY(RHand));
@@ -24,11 +34,11 @@ void Robot::swerveDrive(int mode) {
     float targetSpeed[4], turnMagnitude[4];
     float vector1[2], vector2[4][2], finalVector[4][2];
     if (mode == 0) {
-        targetEncoder[0] = (int)(lAngle / 360 * encoders);
+        targetEncoder[0] = (int)(lAngle / 360 * ENCODERS);
         targetEncoder[1] = targetEncoder[0];
         targetEncoder[2] = targetEncoder[0];
         targetEncoder[3] = targetEncoder[0];
-        targetSpeed[0] = speed * (((*control1).GetTriggerAxis(RHand) - (*control1).GetTriggerAxis(LHand)));
+        targetSpeed[0] = SPEED * (((*control1).GetTriggerAxis(RHand) - (*control1).GetTriggerAxis(LHand)));
         if (targetSpeed[0] < 0.3 && targetSpeed[0] > -0.3)
             targetSpeed[0] = 0;
         targetSpeed[1] = targetSpeed[0];
@@ -36,11 +46,11 @@ void Robot::swerveDrive(int mode) {
         targetSpeed[3] = targetSpeed[0];
     }
     if (mode == 1 || mode == 4) {
-        targetEncoder[0] = (int)(rAngle / 360 * encoders);
+        targetEncoder[0] = (int)(rAngle / 360 * ENCODERS);
         targetEncoder[1] = targetEncoder[0];
         targetEncoder[2] = targetEncoder[0];
         targetEncoder[3] = targetEncoder[0];
-        targetSpeed[0] = (distCalc((*control1).GetX(RHand), 0-(*control1).GetY(RHand))) * speed;
+        targetSpeed[0] = (distCalc((*control1).GetX(RHand), 0-(*control1).GetY(RHand))) * SPEED;
         targetSpeed[1] = targetSpeed[0];
         targetSpeed[2] = targetSpeed[0];
         targetSpeed[3] = targetSpeed[0];
@@ -60,11 +70,11 @@ void Robot::swerveDrive(int mode) {
         turnMagnitude[3] = turnMagnitude[2] + 90;
     }
     if (mode == 2 || mode == 3) {
-        targetEncoder[0] = (int)(turnMagnitude[0] / 360 * encoders);
-        targetEncoder[1] = (int)((turnMagnitude[1]) / 360 * encoders);
-        targetEncoder[2] = (int)((turnMagnitude[2]) / 360 * encoders);
-        targetEncoder[3] = (int)((turnMagnitude[3]) / 360 * encoders) % 40; // "% 40" to loop 40 back to 0
-        targetSpeed[0] = speed;
+        targetEncoder[0] = (int)(turnMagnitude[0] / 360 * ENCODERS);
+        targetEncoder[1] = (int)((turnMagnitude[1]) / 360 * ENCODERS);
+        targetEncoder[2] = (int)((turnMagnitude[2]) / 360 * ENCODERS);
+        targetEncoder[3] = (int)((turnMagnitude[3]) / 360 * ENCODERS) % 40; // "% 40" to loop 40 back to 0
+        targetSpeed[0] = SPEED;
         targetSpeed[1] = targetSpeed[0];
         targetSpeed[2] = targetSpeed[0];
         targetSpeed[3] = targetSpeed[0];
@@ -74,9 +84,7 @@ void Robot::swerveDrive(int mode) {
         //vector math shit
         vector1[0] = (*control1).GetX(RHand);
         vector1[1] = 0-(*control1).GetY(RHand);
-        vector1[0] /= 32767;
-        vector1[1] /= 32767;
-        vector2[0][0] = cosf(turnMagnitude[0]);
+        vector2[0][0] = cosf(turnMagnitude[0]); //Scale from degrees to values 0-1 for X and Y
         vector2[0][1] = sinf(turnMagnitude[0]);
         vector2[1][0] = cosf(turnMagnitude[1]);
         vector2[1][1] = sinf(turnMagnitude[1]);
@@ -98,38 +106,38 @@ void Robot::swerveDrive(int mode) {
         targetEncoder[1] = angleCalc(finalVector[1][0], finalVector[1][1]);
         targetEncoder[2] = angleCalc(finalVector[2][0], finalVector[2][1]);
         targetEncoder[3] = angleCalc(finalVector[3][0], finalVector[3][1]);
-        targetSpeed[0] = distCalc(finalVector[0][0], finalVector[0][1]) / 2 * speed;
-        targetSpeed[1] = distCalc(finalVector[1][0], finalVector[1][1]) / 2 * speed;
-        targetSpeed[2] = distCalc(finalVector[2][0], finalVector[2][1]) / 2 * speed;
-        targetSpeed[3] = distCalc(finalVector[3][0], finalVector[3][1]) / 2 * speed;
+        targetSpeed[0] = distCalc(finalVector[0][0], finalVector[0][1]) / 2 * SPEED;
+        targetSpeed[1] = distCalc(finalVector[1][0], finalVector[1][1]) / 2 * SPEED;
+        targetSpeed[2] = distCalc(finalVector[2][0], finalVector[2][1]) / 2 * SPEED;
+        targetSpeed[3] = distCalc(finalVector[3][0], finalVector[3][1]) / 2 * SPEED;
     }
 
 
     //Align wheels
-    if ((*flHall).Get() == targetEncoder[0])
+    if (((*flHall).Get() + ENCODERS) % ENCODERS == targetEncoder[0])
         (*flTurn).Set(0);
-    else if (((*flHall).Get() + (encoders / 2)) % encoders > targetEncoder[0])
-        (*flTurn).Set(speed);
+    else if (((*flHall).Get() + (ENCODERS * 1.5)) % ENCODERS < targetEncoder[0])
+        (*flTurn).Set(SPEED);
     else
-        (*flTurn).Set(-speed);
-    if ((*frHall).Get() == targetEncoder[1])
+        (*flTurn).Set(-SPEED);
+    if (((*frHall).Get() + ENCODERS) % ENCODERS == targetEncoder[1])
         (*frTurn).Set(0);
-    else if (((*frHall).Get() + (encoders / 2)) % encoders > targetEncoder[1])
-        (*frTurn).Set(speed);
+    else if (((*frHall).Get() + (ENCODERS * 1.5)) % ENCODERS < targetEncoder[1])
+        (*frTurn).Set(SPEED);
     else
-        (*frTurn).Set(-speed);
-    if ((*blHall).Get() == targetEncoder[2])
+        (*frTurn).Set(-SPEED);
+    if (((*blHall).Get() + ENCODERS) % ENCODERS == targetEncoder[2])
         (*blTurn).Set(0);
-    else if (((*blHall).Get() + (encoders / 2)) % encoders > targetEncoder[2])
-        (*blTurn).Set(speed);
+    else if (((*blHall).Get() + (ENCODERS * 1.5)) % ENCODERS < targetEncoder[2])
+        (*blTurn).Set(SPEED);
     else
-        (*blTurn).Set(-speed);
-    if ((*brHall).Get() == targetEncoder[3])
+        (*blTurn).Set(-SPEED);
+    if (((*brHall).Get() + ENCODERS) % ENCODERS == targetEncoder[3])
         (*brTurn).Set(0);
-    else if (((*brHall).Get() + (encoders / 2)) % encoders > targetEncoder[3])
-        (*brTurn).Set(speed);
+    else if (((*brHall).Get() + (ENCODERS * 1.5)) % ENCODERS < targetEncoder[3])
+        (*brTurn).Set(SPEED);
     else
-        (*brTurn).Set(-speed);
+        (*brTurn).Set(-SPEED);
     
     //Move
     (*flMain).Set(targetSpeed[0]);
